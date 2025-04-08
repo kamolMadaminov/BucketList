a68c1e6cc00318f4dc48eabc9c009b0687550852
developerkamol <developerkamol@gmail.com>
2025-04-08 21:05:58 +0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kamol Madaminov on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
